d1e38e73ba1f6a8cd6c24096b3dc830f29b7f5a3
PushkarShinde <pushkarshinde249@gmail.com>
2025-05-01 18:08:33 +0530

    Comments added to the static method file

--- src/StaticMembers/Greeting.java
+++ src/StaticMembers/Greeting.java
@@ -7,13 +7,13 @@ public class Greeting {
     }
 
     public static void index(int[] arr, int t){
-        BS in=new BS();
+        BS in=new BS(); // pehle ek object banaya then we used it's methods and attributes.
         int ind=BS.binarySearch(arr,t);
         System.out.println("a["+ind+"]");
     }
 
     public static void main(String[] args) {
         int[] arr={1,2,3,4,55,543,4,2,342,2312};
-        Greeting.index(arr,55);
+        Greeting.index(arr,55); // static method can be called by the class name itself
     }
 }
